ad4d23c441c611e3af8db7c5626f0e9dc645f10f
JoaoBarreto03 <211039519@aluno.unb.br>
2023-03-15 19:42:11 -0300

    Criação do form de serviços + Exceção para o serviço não ultrapassar o orçamento

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,13 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { parse, v4 as uuidv4 } from 'uuid'
 
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
 import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
+import ServiceForm from '../service/ServiceForm'
 
 function Project() {
     const { id } = useParams()
@@ -36,7 +38,7 @@ function Project() {
 
         //budget validation
         if(project.budget < project.cost) {
-            setMessage('O orçamento não pode ser menor que o custo do projeto')
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
             return false
         }
@@ -56,6 +58,43 @@ function Project() {
         })
         .catch(Err => console.log())
 
+    }
+
+    function createService(project) {
+        setMessage('')
+        //last service
+        const lastService = project.services[project.services.length - 1]
+        lastService.id = uuidv4()
+        const lastServiceCost = lastService.cost
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        //maximun value validation
+
+        if(newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+            setType('error')
+            project.services.pop()
+            return false
+
+        }
+
+        //add service cost to project total cost
+        project.cost = newCost
+
+        //update project
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            //exibir os serviços
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+   
     }
 
     function toggleProjectForm() {
@@ -104,7 +143,14 @@ function Project() {
                                 {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
                             </button>
                             <div className={styles.project_info}>
-                                {showServiceForm && <div>formulário do serviço</div>}
+                                {showServiceForm && (
+                                    <ServiceForm
+                                        handleSubmit={createService}
+                                        btnText="Adicionar Serviço"
+                                        projectData={project}
+
+                                    />
+                                )}
                             </div>
                         </div>
                         <h2>Serviços</h2>
